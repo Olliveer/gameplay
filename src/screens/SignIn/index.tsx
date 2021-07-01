f9bb2032f1460b61d8631afa57f3817b4461ca15
José Oliveira <j.oliveira90@live.com>
2021-07-01 13:39:51 -0300

    add Profile component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,49 +1,34 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  Image,
-  StatusBar,
-} from 'react-native';
-
-import { style } from './styles';
-import { ButtonIcon } from '../../components/ButtonIcon';
+import { Image, Text, View } from 'react-native';
 import IlustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { style } from './styles';
 
 export function SignIn() {
-  return (
-    <View style={style.containter}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Image
-        source={IlustrationImg}
-        style={style.image}
-        resizeMode="stretch"
-      />
+    return (
+        <View style={style.containter}>
+            <Image
+                source={IlustrationImg}
+                style={style.image}
+                resizeMode="stretch"
+            />
 
-      <View style={style.content}>
-        <Text style={style.title}>
-          Conecte-se
-          {'\n'}
-          e organize suas
-          {'\n'}
-          jogatinas
-        </Text>
+            <View style={style.content}>
+                <Text style={style.title}>
+                    Conecte-se
+                    {'\n'}e organize suas
+                    {'\n'}
+                    jogatinas
+                </Text>
 
-        <Text style={style.subTitle}>
-          Crie grupos para jogar seus games
-          {'\n'}
-          favoritos com seus amigos
-        </Text>
+                <Text style={style.subTitle}>
+                    Crie grupos para jogar seus games
+                    {'\n'}
+                    favoritos com seus amigos
+                </Text>
 
-        <ButtonIcon
-          title="Entrar com o Discord"
-          activeOpacity={0.7}
-        />
-      </View>
-    </View>
-  );
+                <ButtonIcon title="Entrar com o Discord" activeOpacity={0.7} />
+            </View>
+        </View>
+    );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -2,31 +2,33 @@ import { StyleSheet } from 'react-native';
 import { theme } from '../../global/styles/theme';
 
 export const style = StyleSheet.create({
-  containter: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: theme.colors.background,
-  },
-  image: {
-    width: '100%',
-    height: 360,
-  },
-  content: {
-    marginTop: -40,
-    padding: 50,
-  },
-  title: {
-    color: theme.colors.heading,
-    textAlign: 'center',
-    fontSize: 40,
-    marginBottom: 16,
-  },
-  subTitle: {
-    color: theme.colors.heading,
-    fontSize: 15,
-    textAlign: 'center',
-    marginBottom: 64,
-  },
-
+    containter: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: '100%',
+        height: 360,
+    },
+    content: {
+        marginTop: -40,
+        padding: 50,
+    },
+    title: {
+        color: theme.colors.heading,
+        textAlign: 'center',
+        fontSize: 40,
+        marginBottom: 16,
+        fontFamily: theme.fonts.title700,
+        lineHeight: 40,
+    },
+    subTitle: {
+        color: theme.colors.heading,
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 64,
+        fontFamily: theme.fonts.text500,
+        lineHeight: 25,
+    },
 });
