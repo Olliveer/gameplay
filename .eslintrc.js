08ef9f547e4586f807c1aab1527fb52438669e44
José Oliveira <j.oliveira90@live.com>
2021-07-01 13:39:07 -0300

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    'react-native/react-native': true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-
+    env: {
+        browser: true,
+        'react-native/react-native': true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'react-native',
-  ],
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
-    'no-use-before-define': 'off',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'react/jsx-props-no-spreading': 'off',
-  },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'react-native'],
+    rules: {
+        'react/jsx-filename-extension': [
+            1,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
+        camelcase: 'off',
+        'no-use-before-define': 'off',
+        'import/extensions': 'off',
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'react/jsx-props-no-spreading': 'off',
+    },
 };
